532156ab04e071a5b074b4f7d70ad1b1124df856
akivaab <amabramo18@gmail.com>
2023-03-21 20:38:43 +0200

    section3 start

---
+++ section3/script.js
@@ -0,0 +1,5 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const canvasWidth = canvas.width = 500;
+const canvasHeight = canvas.height = 1000;
