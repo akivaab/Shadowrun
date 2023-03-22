0d87f8e00089eabee70babf9bde8aedeb3a89172
akivaab <amabramo18@gmail.com>
2023-03-22 20:46:03 +0200

    enemy movement framework

--- section3/script.js
+++ section3/script.js
@@ -3,3 +3,35 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const canvasWidth = canvas.width = 500;
 const canvasHeight = canvas.height = 1000;
+const enemyArray = [];
+const numberOfEnemies = 100;
+
+class Enemy {
+    constructor() {
+        this.x = Math.random() * canvasWidth;
+        this.y = Math.random() * canvasHeight;
+        this.width = 100;
+        this.height = 100;
+        this.speed = Math.random() * 2 - 1;
+    }
+    update() {
+        this.x += this.speed;
+        this.y += this.speed;
+    }
+    draw() {
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+};
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemyArray.push(new Enemy());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    enemyArray.forEach(enemy => { 
+        enemy.update();
+        enemy.draw();
+    });
+    requestAnimationFrame(animate);
+}
+animate();
