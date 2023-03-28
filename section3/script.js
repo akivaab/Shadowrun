22d3a879e8ac4511d2e5ab9a9a436172c17ce510
akivaab <amabramo18@gmail.com>
2023-03-28 21:12:33 +0300

    enter the enemy

--- section3/script.js
+++ section3/script.js
@@ -5,21 +5,32 @@ const canvasWidth = canvas.width = 500;
 const canvasHeight = canvas.height = 1000;
 const enemyArray = [];
 const numberOfEnemies = 100;
+let gameFrame = 0;
 
 class Enemy {
     constructor() {
+        this.image = new Image();
+        this.image.src = 'enemy1.png';
         this.x = Math.random() * canvasWidth;
         this.y = Math.random() * canvasHeight;
-        this.width = 100;
-        this.height = 100;
-        this.speed = Math.random() * 2 - 1;
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
         this.x += this.speed;
         this.y += this.speed;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
     }
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,
+            this.x, this.y, this.width, this.height);
     }
 };
 for (let i = 0; i < numberOfEnemies; i++) {
@@ -32,6 +43,7 @@ function animate() {
         enemy.update();
         enemy.draw();
     });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
