0cafc555da65803d1feb37232cce0d5da939081d
akivaab <amabramo18@gmail.com>
2023-06-19 21:03:40 +0300

    new ravens at half second intervals

--- section5/script.js
+++ section5/script.js
@@ -4,6 +4,10 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+
 let ravens = [];
 class Raven {
     constructor() {
@@ -24,7 +28,15 @@ class Raven {
 
 function animate(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    let deltatime = timestamp - lastTime;
+    lastTime = timestamp;
+    timeToNextRaven += deltatime;
+    if (timeToNextRaven > ravenInterval) {
+        ravens.push(new Raven());
+        timeToNextRaven = 0;
+    }
+    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.draw());
     requestAnimationFrame(animate);
 }
-animate();
+animate(0);
