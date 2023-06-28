5f35a1112f991d91b796ea517d8d9e17085467f8
akivaab <amabramo18@gmail.com>
2023-06-28 20:57:37 +0300

    section 6 continued

--- section6/script.js
+++ section6/script.js
@@ -1,42 +1,61 @@
 /** @type {HTMLCanvasElement} */
-document.addEventListener('load', function() {
+window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
     canvas.height = 800;
 
     class Game {
-        constructor() {
+        constructor(ctx, width, height) {
+            this.ctx = ctx;
+            this.width = width;
+            this.height = height;
             this.enemies = [];
+            this.enemyInterval = 1000;
+            this.enemyTimer = 0;
         }
-        update() {
-
+        update(deltaTime) {
+            if (this.enemyTimer > this.enemyInterval) {
+                this.#addNewEnemy();
+                this.enemyTimer = 0;
+            }
+            else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => enemy.update());
         }
         draw() {
-
+            this.enemies.forEach(enemy => enemy.draw(this.ctx));
         }
         #addNewEnemy() {
-
+            this.enemies.push(new Enemy(this));
         }
     }
 
     class Enemy {
-        constructor() {
-
+        constructor(game) {
+            this.game = game;
+            this.x = this.game.width;
+            this.y = Math.random() * this.game.height;
+            this.width = 100;
+            this.height = 100;
         }
         update() {
-
+            this.x--;
         }
-        draw() {
-
+        draw(ctx) {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
 
+    const game = new Game(ctx, canvas.width, canvas.height);
     let lastTime = 1;
     function animate(timeStamp) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         const deltaTime = timeStamp - lastTime;
         lastTime = timeStamp;
+        game.update(deltaTime);
+        game.draw();
         requestAnimationFrame(animate);
     }
     animate(0);
