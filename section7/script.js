99d166d0f72da8188c63a3894abb49d26587bd7e
akivaab <amabramo18@gmail.com>
2023-07-04 21:22:15 +0300

    section 7 begins

---
+++ section7/script.js
@@ -0,0 +1,7 @@
+/** @type {HTMLCanvasElement} */
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+});
